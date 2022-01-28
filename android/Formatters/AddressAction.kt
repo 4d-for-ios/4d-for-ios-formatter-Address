aec8bb953e47cdd0997e297f3832a80cb96412cc
Quentin Marciset <quentin.marciset@4d.com>
2022-01-28 14:54:17 +0100

    Improve location parsing

--- android/Formatters/AddressAction.kt
+++ android/Formatters/AddressAction.kt
@@ -4,13 +4,40 @@ import android.content.Intent
 import android.net.Uri
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.qmobile.qmobileapi.utils.UTF8
+import com.qmobile.qmobiledatasync.toast.MessageType
+import com.qmobile.qmobileui.utils.ToastHelper
+import timber.log.Timber
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
 
 @BindingAdapter("addressAction")
 fun addressAction(view: TextView, addressAction: String?) {
     if (addressAction.isNullOrEmpty()) return
-
+    view.text = addressAction
     view.setOnClickListener {
-        val gmmIntentUri = Uri.parse("geo:$addressAction")
+
+        @Suppress("MaxLineLength")
+        val lonLatRegex =
+            "^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?),\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)\$".toRegex()
+        val uri = if (lonLatRegex.matches(addressAction)) {
+            "geo:$addressAction"
+        } else {
+            try {
+                val query = URLEncoder.encode(addressAction, UTF8)
+                "geo:0,0?q=$query"
+            } catch (e: UnsupportedEncodingException) {
+                Timber.e(e.localizedMessage)
+                ToastHelper.show(
+                    view.context,
+                    "Could not encode given address for Maps",
+                    MessageType.WARNING
+                )
+                return@setOnClickListener
+            }
+        }
+
+        val gmmIntentUri = Uri.parse(uri)
         val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
         mapIntent.setPackage("com.google.android.apps.maps")
         view.context.startActivity(mapIntent)
